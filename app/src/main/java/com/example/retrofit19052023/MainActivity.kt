0130e5f3d48e02ecf55e2b9a7e9e91ea79d44e41
phamtanphat <phatdroid94@gmail.com>
2023-09-20 19:11:28 +0700

    get post

--- app/src/main/java/com/example/retrofit19052023/MainActivity.kt
+++ app/src/main/java/com/example/retrofit19052023/MainActivity.kt
@@ -2,8 +2,13 @@ package com.example.retrofit19052023
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.google.gson.Gson
 import com.google.gson.GsonBuilder
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
+import kotlinx.coroutines.launch
 import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
@@ -24,5 +29,17 @@ class MainActivity : AppCompatActivity() {
             .baseUrl("https://jsonplaceholder.typicode.com/")
             .client(okhttp)
             .addConverterFactory(GsonConverterFactory.create(GsonBuilder().create()))
+            .build()
+
+        val apiService = retrofit.create(ApiService::class.java)
+
+        CoroutineScope(Dispatchers.IO).launch {
+            try {
+                val listPosts = async { apiService.getPost() }.await()
+                listPosts.forEach { Log.d("BBB", it.toString()) }
+            } catch (e: Exception) {
+
+            }
+        }
     }
 }
